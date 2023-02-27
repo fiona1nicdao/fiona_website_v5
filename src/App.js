8d12aa8fee01efd298a88fba8b4ba6a17f1b1d47
FionaN <fiona1941nicdao@gmail.com>
2023-02-27 10:50:59 -0600

    add footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './assets/css/App.scss';
 import './assets/css/reset.css';
-import { Parallax, ParallaxLayer } from '@react-spring/parallax';
-import { useTrail, a } from '@react-spring/web'
+
+// import { Parallax, ParallaxLayer } from '@react-spring/parallax';
+// import { useTrail, a } from '@react-spring/web'
+
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <h1>Fiona Nicdao</h1>
-      
+      <Footer/>
+
     </div>
   );
 }
