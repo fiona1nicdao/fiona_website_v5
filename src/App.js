110565c02e48ec4a21cc144d0988c4422730cc6c
FionaN <fiona1941nicdao@gmail.com>
2023-02-27 11:00:00 -0600

    add home page and header

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './assets/css/reset.css';
 
 // import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 // import { useTrail, a } from '@react-spring/web'
-
+import Header from './components/header/Header';
+import Home from './pages/home/Home';
 import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <h1>Fiona Nicdao</h1>
+      <Header/>
+      <Home/>
       <Footer/>
 
     </div>
