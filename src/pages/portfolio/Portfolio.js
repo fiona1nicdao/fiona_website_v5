bcba1d27b9a92d2895324b0dfff246fc31f21362
FionaN <fiona1941nicdao@gmail.com>
2023-03-01 00:24:20 -0600

    finished with protfolio section and all the styling

--- src/pages/portfolio/Portfolio.js
+++ src/pages/portfolio/Portfolio.js
@@ -2,9 +2,6 @@ import { useState } from 'react'
 import data from '../../assets/data/data'
 import OnePortfolio from './OnePortfolio'
 
-import foodweatherPIC from '../../assets/images/foodweather.png'
-import weatherPIC from '../../assets/images/weather.png'
-import polariPIC from '../../assets/images/polari.png'
 export default function Portfolio(){
     const [current,setCurrent] = useState(0)
     let length = data.length
@@ -30,44 +27,12 @@ export default function Portfolio(){
                 })}
             </div>
             <div className='arrow-container'>
-                                <div onClick={goBackward}><i class="arrow-icons fa-solid fa-arrow-left-long"></i></div>
-                                <div><i class="dots-icons fa-solid fa-ellipsis"></i></div>
-                                <div onClick={goForward}><i class="arrow-icons fa-solid fa-arrow-right-long"></i></div>
-                            </div>
-            <div className="Projects">
-                <div className="project-1">
-                    <div className="project-content">
-                        <div className="project-image-container">
-                        <img className="project-image" alt="polar website" src={polariPIC}/>
-                        </div>
-                        <div className="project-information">
-                            <div className="project-title"> Polari</div>
-                            <div className='project-content'>
-                                <div className='project-description'>Polari is a dating website caterd to the LGBTQ community with an emphasis to educate users of their attachment styles.</div>
-                                <div className='project-links'>
-                                    <div>
-                                        <a target="_blank" rel="noopener noreferrer" href="https://polari-attachments.herokuapp.com/">
-                                            <i className="project-icons fa-solid fa-desktop"></i>
-                                        </a>
-                                    </div>
-                                    <div>
-                                        <a target="_blank" rel="noopener noreferrer" href="https://github.com/polari-2022">
-                                            <i className="project-icons fa-brands fa-github"></i>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className='arrow-container'>
-                                <div><i class="arrow-icons fa-solid fa-arrow-left-long"></i></div>
-                                <div><i class="dots-icons fa-solid fa-ellipsis"></i></div>
-                                <div><i class="arrow-icons fa-solid fa-arrow-right-long"></i></div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-
+                <div onClick={goBackward}><i class="arrow-icons fa-solid fa-arrow-left-long"></i></div>
+                <div><i class="dots-icons fa-solid fa-ellipsis"></i></div>
+                <div onClick={goForward}><i class="arrow-icons fa-solid fa-arrow-right-long"></i></div>
             </div>
+            <div className='blue-circle'></div>
+            <div className='orange-circle'></div>
         </div>
     )
 }
